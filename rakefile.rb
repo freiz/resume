59b71d7259f5ddd5ed1f631742bd2c119e629f92
freiz <freizsu@gmail.com>
2015-03-26 21:18:29 +0800

    Change the output filename for chinese resume

--- rakefile.rb
+++ rakefile.rb
@@ -9,7 +9,7 @@ task :be => "target" do
 end
 
 task :bc => "target" do
-  sh "cd src; xelatex -output-directory=../target BaojunSu-Resume-Chinese.tex > /dev/null; cd ../target; rm *.aux *.log *.out"
+  sh "cd src; xelatex -output-directory=../target -jobname=苏保君-中文简历 BaojunSu-Resume-Chinese.tex > /dev/null; cd ../target; rm *.aux *.log *.out"
 end
 
 task :je => "target" do
@@ -17,6 +17,6 @@ task :je => "target" do
 end
 
 task :jc => "target" do
-  sh "cd src; xelatex -output-directory=../target JingZhao-Resume-Chinese.tex > /dev/null; cd ../target; rm *.aux *.log *.out"
+  sh "cd src; xelatex -output-directory=../target -jobname=赵晶-中文简历 JingZhao-Resume-Chinese.tex > /dev/null; cd ../target; rm *.aux *.log *.out"
 end
 
